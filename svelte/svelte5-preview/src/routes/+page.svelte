2f0a8985428610dc0663fd6df54315acb4209b6e
Joxtacy <10127673+Joxtacy@users.noreply.github.com>
2024-07-18 21:55:55 +0200

    feat(svelte): add svelte folder

---
+++ svelte/svelte5-preview/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	let x = $state(0);
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+<button on:click={() => x++}>{x}</button>
