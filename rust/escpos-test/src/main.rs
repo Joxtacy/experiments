ad70a718e1caee46ef7eb650770e9393718e2d12
Joxtacy <10127673+Joxtacy@users.noreply.github.com>
2024-10-27 19:53:17 +0100

    feat(rust)!: replace `escpos-rs` with `escpos`

--- rust/escpos-test/src/main.rs
+++ rust/escpos-test/src/main.rs
@@ -1,86 +1,85 @@
-use escpos_rs::{
-    command::{Command, Font},
-    Instruction, Justification, PrintData, Printer, PrinterProfile,
-};
+use escpos::errors::PrinterError;
+use escpos::printer::Printer;
+use escpos::printer_options::PrinterOptions;
+use escpos::utils::*;
+use escpos::{driver::*, errors::Result};
 
-fn main() {
-    // We create a usb contest for the printer
-    let printer_profile = PrinterProfile::usb_builder(0x0483, 0x5743)
-        .with_font_width(Font::FontA, 48)
-        .with_font_width(Font::FontB, 60) // FontB-E are the same
-        .with_font_width(Font::FontC, 60)
-        .with_font_width(Font::FontD, 60)
-        .with_font_width(Font::FontE, 60)
-        .build();
-    // We pass it to the printer
-    let printer = match Printer::new(printer_profile) {
-        Ok(maybe_printer) => match maybe_printer {
-            Some(printer) => printer,
-            None => panic!("No printer was found :("),
-        },
-        Err(e) => panic!("Error: {}", e),
-    };
+fn main() -> Result<()> {
+    env_logger::init();
 
-    // We print simple text
-    match printer.println("Hello, world! This is a very long sentence that will hopefully break the line. And if it doesn't then I am probably printing out in thin air. Which would be pretty cool to be fair.") {
-        Ok(_) => (),
-        Err(e) => println!("Error: {}", e),
-    }
-
-    // We create a simple instruction with a single substitution
-    let instruction = Instruction::text(
-        "Hello, %name%!",
-        Font::FontA,
-        Justification::Center,
-        // Words that will be replaced in this specific instruction
-        Some(vec!["%name%".into()].into_iter().collect()),
-    );
-    // We create custom information for the instruction
-    let print_data_1 = PrintData::builder().replacement("%name%", "Jesper").build();
-    // And a second set...
-    let print_data_2 = PrintData::builder().replacement("%name%", "Jacob").build();
-
+    // let driver = ConsoleDriver::open(true);
+    let driver = UsbDriver::open(0x0483, 0x5743, None).unwrap();
+    let mut printer_options = PrinterOptions::default();
+    printer_options.characters_per_line(48);
+    let mut printer = Printer::new(driver, Protocol::default(), Some(printer_options));
+    printer.debug_mode(Some(DebugMode::Hex)).init()?;
     printer
-        .instruction(&Instruction::command(Command::Underline1Dot), None)
-        .unwrap();
-    // We send the instruction to the printer, along with the custom data
-    // for this particular print
-    match printer.instruction(&instruction, Some(&print_data_1)) {
-        Ok(_) => (), // "Hello, Jesper!" should've been printed.
-        Err(e) => println!("Error: {}", e),
-    }
-    printer
-        .instruction(&Instruction::command(Command::Underline2Dot), None)
-        .unwrap();
-    printer
-        .instruction(&Instruction::command(Command::BoldOn), None)
-        .unwrap();
-    // Now we print the second data
-    match printer.instruction(&instruction, Some(&print_data_2)) {
-        Ok(_) => (), // "Hello, Jacob!" should've been printed.
-        Err(e) => println!("Error: {}", e),
-    }
-    printer
-        .instruction(&Instruction::command(Command::BoldOff), None)
-        .unwrap();
-    printer
-        .instruction(&Instruction::command(Command::UnderlineOff), None)
-        .unwrap();
+        // check status
+        .real_time_status(RealTimeStatusRequest::Printer)?
+        .real_time_status(RealTimeStatusRequest::RollPaperSensor)?
+        .send_status()?
+        // text
+        .smoothing(true)?
+        .bold(true)?
+        .underline(UnderlineMode::Single)?
+        .writeln("Bold underline")?
+        .justify(JustifyMode::CENTER)?
+        .reverse(true)?
+        .bold(false)?
+        .writeln("Hello world - Reverse")?
+        .feed()?
+        .justify(JustifyMode::RIGHT)?
+        .reverse(false)?
+        .underline(UnderlineMode::None)?
+        .size(2, 3)?
+        .writeln("Hello world - Normal")?
+        // QR code
+        // bit image
+        .bit_image_option(
+            "./resources/images/rust-logo-small.png",
+            BitImageOption::new(Some(128), None, BitImageSize::Normal)?,
+        )?
+        .feed()?
+        .print()?;
+    //.print_cut()?;
 
-    let qr_instruction = Instruction::dynamic_qr_code("My QR");
-    let print_data_3 = PrintData::builder()
-        .add_qr_code("My QR", "https://youtu.be/dQw4w9WgXcQ")
-        .build();
+    printer.justify(JustifyMode::CENTER)?;
+    print_ean13_code(&mut printer, "1234567890123".into())?;
 
-    // We print a QR code
-    match printer.instruction(&qr_instruction, Some(&print_data_3)) {
-        Ok(_) => (), // a QR code should've been printed
-        Err(e) => println!("Error: {}", e),
-    }
+    printer.justify(JustifyMode::LEFT)?;
+    print_qr_code(&mut printer, "https://google.com".into())?;
 
-    // We cut the paper
-    match printer.instruction(&Instruction::Cut, None) {
-        Ok(_) => (), // cut
-        Err(e) => println!("Error: {}", e),
-    }
+    Ok(())
+}
+
+fn print_ean13_code(
+    printer: &mut Printer<UsbDriver>,
+    data: String,
+) -> Result<&mut Printer<UsbDriver>> {
+    printer
+        .ean13_option(
+            data.as_str(),
+            BarcodeOption::new(
+                BarcodeWidth::M,
+                BarcodeHeight::S,
+                BarcodeFont::A,
+                BarcodePosition::Below,
+            ),
+        )?
+        .print()?
+        .feed()
+}
+
+fn print_qr_code(
+    printer: &mut Printer<UsbDriver>,
+    data: String,
+) -> Result<&mut Printer<UsbDriver>> {
+    // let driver = ConsoleDriver::open(true);
+    printer
+        .qrcode_option(
+            data.as_str(),
+            QRCodeOption::new(QRCodeModel::Model1, 6, QRCodeCorrectionLevel::M),
+        )?
+        .print()?
+        .feed()
 }
